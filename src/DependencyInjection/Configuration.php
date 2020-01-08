1d1fd7e8f952d6b76352a9dfd696b93ff28de669
kot <kostiaGm@gmail.com>
2020-01-08 15:02:48 +0200

    FCЭ Э

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Symka\Content\DependencyInjection;
+
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('symka_content');
+        return $treeBuilder;
+    }
+}
